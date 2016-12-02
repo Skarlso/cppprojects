bf9779eb317547c53d56ea8f5f4299300e9c8ce9
Gergely Brautigam <skarlso777@gmail.com>
2016-12-02 22:31:29 +0100

    Refining the code.

--- advent-of-code-2016/day-02_part1/main.cpp
+++ advent-of-code-2016/day-02_part1/main.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <vector>
 
+struct coord_t {
+    int v;
+    int h;
+} coord;
+
 std::string get_file_contents(const char *filename)
 {
   std::ifstream in(filename, std::ios::in | std::ios::binary);
@@ -15,37 +20,40 @@ std::string get_file_contents(const char *filename)
   throw(errno);
 }
 
-
 int main(int argc, char* argv[]) {
     int keypad[][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
     int v = 1;
     int h = 1;
     std::vector<int> combination;
     std::string content = get_file_contents(argv[1]);
-    for(char& c : content) {
-        switch(c) {
-            case 'U' : if (v - 1 >= 0) {
-                           v--;
-                       }
-                       break;
-            case 'D' : if (v + 1 < 3) {
-                           v++;
-                       }
-                       break;
-            case 'L' : if (h - 1 >= 0) {
-                           h--;
-                       }
-                       break;
-            case 'R' : if (h + 1 < 3) {
-                           h++;
-                       }
-                       break;
-            case '\n' : combination.push_back(keypad[v][h]);
-                        break;
+    for(auto c : content) {
+        if (c == 'U') {
+            coord.v = -1;
+            coord.h = 0;
+        } else if (c == 'D') {
+            coord.v = 1;
+            coord.h = 0;
+        } else if (c == 'R') {
+            coord.v = 0;
+            coord.h = 1;
+        } else if (c == 'L') {
+            coord.v = 0;
+            coord.h = -1;
+        } else if (c == '\n') {
+            combination.push_back(keypad[v][h]);
+            continue;
+        }
+
+        if (v + coord.v >= 0 && v + coord.v < 3) {
+            v += coord.v;
+        }
+        if (h + coord.h >= 0 && h + coord.h < 3) {
+            h += coord.h;
         }
     }
+
     for (auto i : combination) {
         std::cout << i;
     }
-    return 0;
 }
+
