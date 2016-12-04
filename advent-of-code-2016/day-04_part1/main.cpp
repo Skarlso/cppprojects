e38ea6b5369c97ea6ec8e5e100e00afcc6001333
Gergely Brautigam <skarlso777@gmail.com>
2016-12-04 17:03:10 +0100

    Refactoring for better understanding.

--- advent-of-code-2016/day-04_part1/main.cpp
+++ advent-of-code-2016/day-04_part1/main.cpp
@@ -26,30 +26,44 @@ std::string extract_five_most_commond_letters(std::map<char, int> letters) {
     return common.substr(0, 5);
 }
 
+std::string extract_needed_letters(std::string str) {
+    std::size_t begin = str.find("[");
+    std::string needed_letters = str.substr(begin+1);
+    needed_letters.pop_back();
+    return needed_letters;
+}
+
+int extract_id(std::string str) {
+    std::string end = str.substr(str.find_last_of("-")+1, str.length());
+    std::stringstream ss(end);
+    int id;
+    ss >> id;
+    return id;
+}
+
+std::map<char, int> extract_letter_count(std::string str) {
+    std::map<char, int> ret;
+    str = str.substr(0, str.find_last_of("-"));
+    for (char& c : str) {
+        // Counting number of letter occurances
+        if (c != '-') {
+            ret[c]++;
+        }
+    }
+    return ret;
+}
+
 int main(int argc, char* argv[]) {
     std::ifstream file(argv[1]);
     std::string str; 
     int valid_count = 0;
     while (std::getline(file, str)) {
-        std::map <char, int> letters;
         boost::trim(str);
         // Extracting what letters are needed by getting the last element
         // Regex didn't work. Falling back to substring of some weird architecture error... :/
-        std::size_t begin = str.find("[");
-        std::string needed_letters = str.substr(begin+1);
-        needed_letters.pop_back();
-        std::string end = str.substr(str.find_last_of("-")+1, str.length());
-        std::stringstream ss(end);
-        int id;
-        ss >> id;
-        std::cout << "ID: " << id << '\n';
-        str = str.substr(0, str.find_last_of("-"));
-        for (char& c : str) {
-            // Counting number of letter occurances
-            if (c != '-') {
-                letters[c]++;
-            }
-        }
+        std::string needed_letters = extract_needed_letters(str);
+        int id = extract_id(str);
+        std::map <char, int> letters = extract_letter_count(str);
         // Extracting 5 most commond letters in alphabetical order
         std::string common = extract_five_most_commond_letters(letters);
         if (common == needed_letters) {
