f84254c5a2584eac3d0fba5ef471fad3453950ba
Gergely Brautigam <skarlso777@gmail.com>
2016-12-05 10:22:35 +0100

    Working MD5 digest.

--- advent-of-code-2016/day-05_part1/main.cpp
+++ advent-of-code-2016/day-05_part1/main.cpp
@@ -1,11 +1,29 @@
 #define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
-#include <cryptopp/md5.h>
+#include "cryptopp/md5.h"
+#include "cryptopp/hex.h"
 #include <string>
 #include <iostream>
 
 const std::string input = "reyedfim";
 
+std::string generateMD5Checksum(std::string message) {
+    byte digest[ CryptoPP::Weak::MD5::DIGESTSIZE ];
+
+    CryptoPP::Weak::MD5 hash;
+    hash.CalculateDigest( digest, (const byte*)message.c_str(), message.length() );
+
+    CryptoPP::HexEncoder encoder;
+    std::string output;
+
+    encoder.Attach( new CryptoPP::StringSink( output ) );
+    encoder.Put( digest, sizeof(digest) );
+    encoder.MessageEnd();
+
+    return output;
+}
+
 int main() {
 
-    std::cout << "Done." << std::endl;
+    std::cout << generateMD5Checksum(input) << std::endl;
+    return 0;
 }
