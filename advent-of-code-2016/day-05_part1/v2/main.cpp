48bb8892ce83816542c7a84ac0509b793bd2c089
Gergely Brautigam <skarlso777@gmail.com>
2016-12-05 15:31:15 +0100

    Removed that obnoxious md5 hash.

---
+++ advent-of-code-2016/day-05_part1/v2/main.cpp
@@ -0,0 +1,35 @@
+#include <openssl/md5.h>
+#include <string>
+#include <iostream>
+
+std::string generateMD5Checksum(std::string input) {
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*)input.c_str(), input.length(), (unsigned char*)&digest);
+    char mdString[33];
+    for(int i = 0; i < 16; i++)
+        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+    return mdString;
+}
+
+int main(int argc, char* argv[])
+{
+    std::string input = "reyedfim";
+    std::string password = "";
+    int count = 1;
+    for(;;) {
+        std::string temp = input;
+        temp += std::to_string(count);
+        std::string sum = generateMD5Checksum(temp);
+        if (sum.substr(0, 5) == "00000") {
+            std::cout << "Found letter: " << sum[5] << std::endl;
+            password += sum[5];
+        }
+        if (password.length() == 8) {
+            break;
+        }
+        count++;
+    }
+    std::cout << password << "\n";
+    return 0;
+}
