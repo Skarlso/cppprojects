e4a4218be13463b4a87a8ad33cfc9441c7758451
Gergely Brautigam <skarlso777@gmail.com>
2016-12-05 09:46:42 +0100

    Day 5 Part 1.

---
+++ advent-of-code-2016/day-05_part1/main.cpp
@@ -0,0 +1,11 @@
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+#include <cryptopp/md5.h>
+#include <string>
+#include <iostream>
+
+const std::string input = "reyedfim";
+
+int main() {
+
+    std::cout << "Done." << std::endl;
+}
