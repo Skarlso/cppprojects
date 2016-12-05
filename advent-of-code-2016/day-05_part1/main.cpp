6e4ddfe6ac5f33eab353227e037d7c57c2211c0a
Gergely Brautigam <skarlso777@gmail.com>
2016-12-05 11:14:32 +0100

    Done Day 5 Part 1.

--- advent-of-code-2016/day-05_part1/main.cpp
+++ advent-of-code-2016/day-05_part1/main.cpp
@@ -6,20 +6,27 @@
 
 const std::string input = "reyedfim";
 
-std::string generateMD5Checksum(std::string message) {
-    byte digest[ CryptoPP::Weak::MD5::DIGESTSIZE ];
+//std::string generateMD5Checksum(std::string message) {
+//    byte digest[ CryptoPP::Weak::MD5::DIGESTSIZE ];
+//
+//    CryptoPP::Weak::MD5 hash;
+//    hash.CalculateDigest( digest, (const byte*)message.c_str(), message.length() );
+//
+//    CryptoPP::HexEncoder encoder;
+//    std::string output;
+//
+//    encoder.Attach( new CryptoPP::StringSink( output ) );
+//    encoder.Put( digest, sizeof(digest) );
+//    encoder.MessageEnd();
+//
+//    return output;
+//}
 
+std::string generateMD5ChecksumWithFilter(std::string message) {
     CryptoPP::Weak::MD5 hash;
-    hash.CalculateDigest( digest, (const byte*)message.c_str(), message.length() );
-
-    CryptoPP::HexEncoder encoder;
-    std::string output;
-
-    encoder.Attach( new CryptoPP::StringSink( output ) );
-    encoder.Put( digest, sizeof(digest) );
-    encoder.MessageEnd();
-
-    return output;
+    std::string digest;
+    CryptoPP::StringSource s(message, true, new CryptoPP::HashFilter(hash, new CryptoPP::HexEncoder(new CryptoPP::StringSink(digest))));
+    return digest;
 }
 
 int main() {
@@ -28,9 +35,9 @@ int main() {
     for(;;) {
         std::string temp = input;
         temp += std::to_string(count);
-        auto sum = generateMD5Checksum(temp);
-        std::cout << sum.substr(0, 5) << std::endl;
+        auto sum = generateMD5ChecksumWithFilter(temp);
         if (sum.substr(0, 5) == "00000") {
+            std::cout << "Found letter: " << sum[5] << std::endl;
             password += sum[5];
         }
         if (password.length() == 8) {
