3cbb77b4aad07a47fb8d028bd645696c301a3968
Gergely Brautigam <skarlso777@gmail.com>
2016-12-10 15:08:16 +0100

    Day 10 Part 2 WIP

---
+++ advent-of-code-2016/day-10_part2/main.cpp
@@ -0,0 +1,115 @@
+#include <fstream>
+#include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdio.h>
+#include <algorithm>
+#include <map>
+
+
+typedef struct chip {
+    int num;
+} chip;
+
+inline bool sortChips(chip c1, chip c2) { return (c1.num < c2.num); }
+
+typedef struct output {
+    int id;
+    std::vector<chip> chips;
+} output;
+
+typedef struct bot {
+    int id;
+    std::vector<chip> chips;
+} bot;
+
+int main(int argc, char* argv[]) {
+    std::ifstream file(argv[1]);
+    std::string s;
+    std::map<int, bot> bots;
+    std::vector<std::string> bot_distribute;
+    std::map<int, output> outputs;
+    while (std::getline(file, s)) {
+        boost::trim(s);
+        std::vector<std::string> split;
+        boost::split(split, s, boost::is_any_of(" "));
+        if (split[0] == "value") {
+            int value, botID;
+            std::sscanf(s.c_str(), "value %d goes to bot %d", &value, &botID);
+            chip c = { value };
+            std::map<int, bot>::iterator it = bots.find(botID);
+            if (it == bots.end()) {
+                // Bot is not yet known
+                bot b;
+                b.id = botID;
+                std::vector<chip> chips;
+                chips.push_back(c);
+                b.chips = chips; 
+                bots[botID] = b;
+            } else {
+                // Bot is known
+                std::vector<chip> chips = it->second.chips;
+                chips.push_back(c);
+                it->second.chips = chips;
+            }
+        } else if (split[0] == "bot") {
+            bot_distribute.push_back(s);
+        }
+    }
+
+    for(auto it = bot_distribute.begin(); it != bot_distribute.end(); ++it) {
+        int botSource, highDesID, lowDesID;
+        char lowDes[10];
+        char highDes[10];
+        std::sscanf(it->c_str(), "bot %d gives low to %s %d and high to %s %d", &botSource, lowDes, &lowDesID, highDes, &highDesID);
+        //std::printf("BotSource: %d; LowDes: %s; LowID: %d, HighDes: %s; HighID: %d \n", botSource, lowDes, lowDesID, highDes, highDesID);
+
+        if (std::string(highDes) == "output") {
+            if (outputs.find(highDesID) == outputs.end()) {
+                std::cout << "Adding output: " << highDesID << '\n';
+                output o;
+                o.id = highDesID;
+                o.chips = std::vector<chip>{};
+                outputs[highDesID] = o;
+            }
+        }
+        if (std::string(lowDes) == "output") {
+            if (outputs.find(lowDesID) == outputs.end()) {
+                std::cout << "Adding output: " << lowDesID<< '\n';
+                output o;
+                o.id = lowDesID;
+                o.chips = std::vector<chip>{};
+                outputs[lowDesID] = o;
+            }
+        }
+        int high, low;
+        bot b = bots[botSource];
+        if (b.chips.size() != 2) {
+            continue;
+        }
+        std::sort(b.chips.begin(), b.chips.end(), sortChips);
+        low = b.chips[0].num;
+        high = b.chips[1].num;
+        //std::printf("Bot: %d is comparing low: %d to high: %d. \n", botSource, low, high);
+        if (std::string(lowDes) == "output") {
+            std::cout << "Low Des ID: " << lowDesID << "\n";
+            outputs[lowDesID].chips.push_back({low});
+        } else {
+            bots[lowDesID].chips.push_back({low});
+        }
+        
+        if (std::string(highDes) == "output") {
+            std::cout << "High Des ID: " << highDesID << "\n";
+            outputs[highDesID].chips.push_back({high});
+        } else {
+            bots[highDesID].chips.push_back({high});
+        }
+
+        b.chips.clear();
+        bots[botSource] = b;
+    }
+
+    std::cout << "Output 0 chip count: " << outputs[0].chips.size() << '\n';
+}
