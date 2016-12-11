2cf70afe2bfb28618f9d103934805a8381d7b31e
Gergely Brautigam <skarlso777@gmail.com>
2016-12-11 22:46:09 +0100

    Working HMAC with openssl right now.

--- totp-generator/src/totp.cpp
+++ totp-generator/src/totp.cpp
@@ -1,22 +1,29 @@
 // Copyright [2016] <Gergely Brautigam>
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include "openssl/hmac.h"
+#include <stdio.h>
+#include <string.h>
+#include <openssl/hmac.h>
 
-using std::string;
-using std::cout;
-using std::copy;
+int main()
+{
+    // The key to hash
+    char key[] = "012345678";
 
-int main(int argc, char* argv[]) {
-    cout << "Number of arguments passed in: " << argc << "\n";
-    // for (int i = 0; i < argc; ++i) {
-    //     std::cout << argv[i] << "\n";
-    // }
-    // return 0;
-    string token = argv[1];
-    unsigned char tokenChar[sizeof(token)];
-    // strcpy(static_cast <char*>(tokenChar), token);
-    copy(token.begin(), token.end(), tokenChar);
-    cout << tokenChar << "\n";
+    // The data that we're going to hash using HMAC
+    char data[] = "hello world";
+    
+    unsigned char* digest;
+    
+    // Using sha1 hash engine here.
+    // You may use other hash engines. e.g EVP_md5(), EVP_sha224, EVP_sha512, etc
+    digest = HMAC(EVP_sha1(), key, strlen(key), (unsigned char*)data, strlen(data), NULL, NULL);    
+
+    // Be careful of the length of string with the choosen hash engine. SHA1 produces a 20-byte hash value which rendered as 40 characters.
+    // Change the length accordingly with your choosen hash engine
+    char mdString[20];
+    for(int i = 0; i < 20; i++)
+         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+
+    printf("HMAC digest: %s\n", mdString);
+
+    return 0;
 }
