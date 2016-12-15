7367d0a85f4a2270f7e970097b56962297ee8709
Gergely Brautigam <skarlso777@gmail.com>
2016-12-15 08:38:14 +0100

    Day 15 Part 1 & 2 DONE.

---
+++ advent-of-code-2016/day-15_part1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+typedef struct disc {
+    int initialPosition;
+    int maxPosition;
+} disc;
+
+
+int main() {
+    vector<disc> discs = {
+        {5, 17},
+        {8, 19},
+        {1, 7},
+        {7, 13},
+        {1, 5},
+        {0, 3},
+    };
+    int planetaryAlignmentLoops = 0;
+    int planetaryAlignment = true;
+    for (int t = 0; ++t;) {
+        int delay = 1;
+        for (auto it = discs.begin(); it != discs.end(); ++it) {
+            int state = (it->initialPosition + delay + t) % it->maxPosition;
+            if (state != 0) {
+                planetaryAlignment = false;
+            }
+            delay++;
+        }
+        if (planetaryAlignment) {
+            planetaryAlignmentLoops = t;
+            break;
+        } 
+        planetaryAlignment = true;
+    }
+
+    std::cout << planetaryAlignmentLoops << '\n';
+}
