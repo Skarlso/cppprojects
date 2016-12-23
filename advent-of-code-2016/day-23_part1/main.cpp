8a25b995594465f8a4d2b168979fc2b4c5c78729
Gergely Brautigam <skarlso777@gmail.com>
2016-12-23 19:18:14 +0100

    Small adjustments.

--- advent-of-code-2016/day-23_part1/main.cpp
+++ advent-of-code-2016/day-23_part1/main.cpp
@@ -12,7 +12,9 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-std::map<std::string, int> registers = {{"a", 7}, {"b", 0}, {"c", 0}, {"d", 0}};
+typedef long long verylong;
+
+std::map<std::string, verylong> registers = {{"a", 7}, {"b", 0}, {"c", 0}, {"d", 0}};
 #define int_or_reg(r) (registers.find(r) != registers.end() ? registers[r] : std::atoi(r.c_str()))
 
 // std::map<std::string, Have here a map of instr<function> -> with lambda.
@@ -30,16 +32,17 @@ int main(int argc, char* argv[]) {
     }
     int atInst = 0;
     for (auto it = instructions.begin(); it != instructions.end();) {
-        // std::cout << "========= I: " << atInst << " =========" << "\n";
-        // for (auto insts = instructions.begin(); insts != instructions.end(); ++insts)
-        // {
-        //     std::cout << insts->at(0) << "\n";
-        // }
-        // if (it->at(0) == "tgl") {
-        //     std::cout << "TOOOOOGLLL" << "\n";
-        // }
-        // int n;
-        // std::cin >> n;
+        //
+        // // std::cout << "========= I: " << atInst << " =========" << "\n";
+        // // for (auto insts = instructions.begin(); insts != instructions.end(); ++insts)
+        // // {
+        // //     std::cout << insts->at(0) << "\n";
+        // // }
+        // // if (it->at(0) == "tgl") {
+        // //     std::cout << "TOOOOOGLLL" << "\n";
+        // // }
+        // // int n;
+        // // std::cin >> n;
         if (it->at(0) == "cpy") {
             string reg = it->at(2);
             string src = it->at(1);
@@ -53,15 +56,15 @@ int main(int argc, char* argv[]) {
             }
         } else if (it->at(0) == "jnz") {
             // std::cout << "JUMPING by: " << it->at(2) << " With reg: " << it->at(1) << "\n";
-            int reg = int_or_reg(it->at(1));
-            int value = int_or_reg(it->at(2));
+            verylong reg = int_or_reg(it->at(1));
+            verylong value = int_or_reg(it->at(2));
             // std::cout << "Values by: " << value << " With reg: " << reg << "\n";
             // if (reg == "a" || reg == "b" || reg == "c" || reg == "d") {
             if (reg > 0) {
                 auto nxt = it;
                 if (value > 0) nxt = std::next(it, value);
                 if (value < 0) nxt = std::prev(it, std::abs(value));
-                if (nxt == instructions.end()) {
+                if (nxt >= instructions.end()) {
                     ++it;
                     ++atInst;
                     continue;
@@ -77,11 +80,11 @@ int main(int argc, char* argv[]) {
             string reg = it->at(1);
             registers[reg]--;
         } else if (it->at(0) == "tgl") {
-            int value = int_or_reg(it->at(1));
+            verylong value = int_or_reg(it->at(1));
             auto nxt = it;
             if (value > 0) nxt = std::next(it, value);
             if (value < 0) nxt = std::prev(it, std::abs(value));
-            if (nxt == instructions.end()) {
+            if (nxt >= instructions.end()) {
                 ++it;
                 ++atInst;
                 continue;
@@ -102,7 +105,6 @@ int main(int argc, char* argv[]) {
         ++it;
         atInst++;
     }
-
     cout << "a: " << registers["a"] << " b: " << registers["b"] << " c: " << registers["c"] << " d: " << registers["d"] << endl;
     return 0;
 }
