cddfc4569c4e4094d097ceb657060294b4c89425
Gergely Brautigam <skarlso777@gmail.com>
2017-02-01 07:28:06 +0100

    THE TIMER IS THE KEY

--- totp-generator/src/totp.cpp
+++ totp-generator/src/totp.cpp
@@ -1,7 +1,7 @@
 // Copyright [2016] <Gergely Brautigam>
 #include <stdio.h>
 #include <string.h>
-#include <openssl/sha.h>
+#include <openssl/hmac.h>
 #include <string>
 #include <ctime>
 #include <cmath>
@@ -9,27 +9,32 @@
 
 using std::string;
 
-string generateOTPTokenV2(string token, std::time_t t) {
+string generateOTPToken(string token, std::time_t t) {
     unsigned char buf[8];
     int timer = int(floor(t/30));
     string decoded = base64_decode("YXNkZg==");
+    // The key to hash
+    char key[] = std::to_string(timer);
+    // The data that we're going to hash using HMAC
+    unsigned char* digest;
+    // Using sha1 hash engine here.
+    // You may use other hash engines. e.g EVP_md5(), EVP_sha224, EVP_sha512, etc
+    digest = HMAC(EVP_sha1(), key, strlen(key), (unsigned char*)token.c_str(), token.length(), NULL, NULL);
 
-    const unsigned char *ibuf = (const unsigned char *)token.c_str();
-    unsigned char obuf[20];
-
-    SHA1(ibuf, token.length(), obuf);
-
+    // Be careful of the length of string with the choosen hash engine. SHA1 produces a 20-byte hash value which rendered as 40 characters.
+    // Change the length accordingly with your choosen hash engine
     char mdString[20];
     for(int i = 0; i < 20; i++)
-         sprintf(&mdString[i*2], "%02x", (unsigned int)obuf[i]);
+         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
 
-    printf("SHA1 digest: %s\n", mdString);
+    printf("HMAC digest: %s\n", mdString);
+
     return "";
 }
 
 int main()
 {
     std::time_t result = std::time(nullptr);
-    generateOTPTokenV2("anyad", result);
+    generateOTPToken("anyad", result);
     return 0;
 }
